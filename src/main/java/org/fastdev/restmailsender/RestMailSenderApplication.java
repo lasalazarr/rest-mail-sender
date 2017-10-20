39c8e4639d923be8d60af5069d3ac5d998eb3e35
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2017-10-19 21:12:07 -0500

    Initial Project Structure
    Initial Project Structure

---
+++ src/main/java/org/fastdev/restmailsender/RestMailSenderApplication.java
@@ -0,0 +1,12 @@
+package org.fastdev.restmailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMailSenderApplication.class, args);
+	}
+}
